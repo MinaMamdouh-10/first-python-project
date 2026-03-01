6fbaad0cf998cfa5858894b9b046c420408a5709
MinaMamdouh-10 <minamamdouhsamir2010@gmail.com>
2026-03-01 04:16:11 +0200

    Update applying-variables.py

--- applying-variables.py
+++ applying-variables.py
@@ -1,4 +1,21 @@
-name ='Mina'
+# Variables: Value (string, integer, float, boolean)
+
+# String: A sequence of characters inside quotes
+name = 'Mina'
+hobby = 'coding'
+food = 'apples'
+print(f'Hello! My name is {name}. I like {hobby} and eating {food}. ')
+
+# Integer: A whole number
 age = 16
-f_hobby = 'coding'
-print(f"Hello, {name}! You are {age} years old and you like {f_hobby}.")
+num = 4
+print(f'I am {age} years old and I have {num} apples. ')
+
+# Float: A number with decimals
+price = 1.50
+print(f'Each apple costs ${price}. ')
+
+# Boolean: True or False statements
+is_graduated = False
+goes_to_school = True
+print(f'Am I graduated?: {is_graduated}.  Do I go to school?: {goes_to_school}. ')
